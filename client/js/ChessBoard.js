0fa8ea9173d8c606ca1b149887b2f390b51fa604
Michael Slezak <michael_slezak08@hotmail.com>
2016-07-13 00:14:05 -0700

    scaling pieces image. adding start of css file and started foundation for chessboard js

---
+++ client/js/ChessBoard.js
@@ -0,0 +1,14 @@
+var ChessBoard = Backbone.View.extend({
+	className: "board",
+	initialize: function() { 
+		this.render();
+	},
+	
+	render: function() {
+		this.$el.appendTo('body');
+	},
+	
+	drawBoard: function() {
+	
+	}
+});
