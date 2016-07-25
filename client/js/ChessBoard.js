648db20bc67961ba24b871f02d972789967b488e
Michael Slezak <michael_slezak08@hotmail.com>
2016-07-25 11:09:50 -0700

    setup the rest of the chess board pieces

--- client/js/ChessBoard.js
+++ client/js/ChessBoard.js
@@ -16,7 +16,7 @@ var ChessBoard = Backbone.View.extend({
 	},
 	
 	renderTiles: function() {
-		var red = true;
+		var red = false;
 		for (var i = 0; i < 8; i++) {
 			for (var j = 0; j < 8; j++) {
 				red = !red;
